d368e7bca46a5bdcf236ad22793898ddf322dddb
kulikovivancw2020@gmail.com <kulikovivancw2020@gmail.com>
2023-11-25 02:39:22 +0300

    Added ProductContext class with models and default values Updated product model Id field

--- Shopping/Shopping.Client/Models/Product.cs
+++ Shopping/Shopping.Client/Models/Product.cs
@@ -2,7 +2,7 @@
 {
     public class Product
     {
-        public required string Id { get; set; }
+        public string Id { get; set; }
         public required string Name { get; set; }
         public required string Category { get; set; }
         public required string Description { get; set; }
