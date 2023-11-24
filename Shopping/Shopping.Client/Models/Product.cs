e3ef7d9ecee2631beaee0641d5fcaaee6a53aa29
kulikovivancw2020@gmail.com <kulikovivancw2020@gmail.com>
2023-11-25 01:42:44 +0300

    Add Product model class

---
+++ Shopping/Shopping.Client/Models/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace Shopping.Client.Models
+{
+    public class Product
+    {
+        public required string Id { get; set; }
+        public required string Name { get; set; }
+        public required string Category { get; set; }
+        public required string Description { get; set; }
+        public required string ImageFile { get; set; }
+        public decimal? Price { get; set; }
+    }
+}
